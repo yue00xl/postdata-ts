9035c29389f84f6cb0edb6f253707d166843bd33
岳小乐 <yuexiaole@hbh.ltd>
2020-12-22 13:57:24 +0800

    first

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 
 
 
-let sum = function(a : string, b: string): string{
-    return a + b;
-}
-
-let nums= sum('水电费说的','对冯绍峰');
-
-console.log(nums)
+var num = function(){
+    console.log(1111)
+},
